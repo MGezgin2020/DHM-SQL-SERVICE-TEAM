8a60d8a068685bd52d73eb3bfb601504cceeb2c2
Murat Gezgin <mgezgin2020@gmail.com>
2025-05-10 23:16:42 +0300

    Update AdventureWorks_DHM_Sql_Team..sql
    just translated some Turkish word into English. No essential changes.. just wording..

--- AdventureWorks_DHM_Sql_Team..sql
+++ AdventureWorks_DHM_Sql_Team..sql
@@ -1,10 +1,10 @@
 SELECT*FROM Sales.Customer
 
---Tüm çalışanların ortalama maaş ortalaması
+--Tüm çalışanların ortalama maaş ortalaması/ Average salary of whole workers . OrtalamaMaas means AverageSalary
 SELECT AVG(Rate) AS OrtalamaMaas
 FROM HumanResources.EmployeePayHistory
 
---1 Ocak 2020'den sonra maaşı değişmiş olan çalışanların ortalama maaşı
+--1 Ocak 2020'den sonra maaşı değişmiş olan çalışanların ortalama maaşı/average salary of the workers whose salaries are modified after 1st January 2020
 SELECT AVG(Rate) AS OrtalamaMaas
 FROM HumanResources.EmployeePayHistory
 WHERE RateChangeDate >= '2020-01-01'
@@ -17,7 +17,7 @@ FROM Sales.CreditCard
 WHERE CardType = 'Visa'
 
 ------------------------------------
---adres city dallas olan kayıtlar
+--adres city dallas olan kayıtlar / Records for the city 'Dallas'
 select*from Person.Address
 
 SELECT *
@@ -25,12 +25,13 @@ FROM Person.Address
 WHERE City = 'Dallas'
 -------------------------------------
 select*from Sales.SalesOrderDetail
---birim fiyatı 250'nin üzerinde olan satış kalemlerini listele
+--birim fiyatı 250'nin üzerinde olan satış kalemlerini listele/ List of the items where the item price is above USD250
 SELECT SalesOrderID, ProductID, UnitPrice, OrderQty
 FROM Sales.SalesOrderDetail
 WHERE UnitPrice > 250
---UnitPrice > 250 olan kayıtların ortalamasını verir
+--UnitPrice > 250 olan kayıtların ortalamasını verir / this is to provide the average of the recordings where the UnitPrice is more than USD250
 
-SELECT AVG(UnitPrice) AS OrtalamaFiyat
+SELECT AVG(UnitPrice) AS OrtalamaFiyat 
 FROM Sales.SalesOrderDetail
 WHERE UnitPrice > 250SELECT * FROM [Production].[ProductCategory]
+-- OrtalamaFiyat = AveragePrice
